75cf8e0390a919bbe52eb98ed679b187b73b47c2
Binzu <33591057+levinciii@users.noreply.github.com>
2023-06-25 16:47:43 +0800

    Additional Nav Links (team, facilities, contact, login)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { TeamComponent } from './team/team.component';
+import { FacilitiesComponent } from './facilities/facilities.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    TeamComponent,
+    FacilitiesComponent,
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
